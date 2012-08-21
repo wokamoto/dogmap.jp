a83fef694270d79a95ce09eb65042bc5704202be
wokamoto <wokamoto1973@gmail.com>
2012-08-21 17:52:39 +0900

    テーマ functions.php の修正

--- wp-content/themes/dogmap2/functions.php
+++ wp-content/themes/dogmap2/functions.php
@@ -14,8 +14,18 @@ load_theme_textdomain(
 // スタイルシートの読み込み
 //**********************************************************************************
 if ( !is_admin() ) {
-    wp_enqueue_style( 'twentytwelve-style', get_template_directory_uri() . '/style.css', array(), '20120725' );
-    wp_enqueue_style( 'twentytwelve-child-style', get_stylesheet_uri(), array('twentytwelve-style'), '20120726' );
+    wp_enqueue_style(
+        'twentytwelve-style',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        date('YmdHis', filemtime(get_template_directory() . '/style.css'))
+        );
+    wp_enqueue_style(
+        'twentytwelve-child-style',
+        get_stylesheet_uri(),
+        array('twentytwelve-style'),
+        date('YmdHis', filemtime(get_stylesheet_directory() . '/style.css'))
+        );
 }
 
 //**********************************************************************************
